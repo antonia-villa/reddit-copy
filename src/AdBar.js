b5c806bc83013504facd25dfab62db5ce1e002a5
pbarbarone <peter.barbarone@gmail.com>
2018-01-25 18:17:49 -0800

    carousel works yo

--- src/AdBar.js
+++ src/AdBar.js
@@ -6,11 +6,59 @@ class AdBar extends Component {
 		return(
 			<div className ="side-bar">
 				<p className="ad-note">advertisement</p>
-				<div className="first-ad"> </div>
+				<div className="first-ad">
+
+	            <div id="carouselIndicators2" className="carousel slide" data-ride="carousel">
+					  <ol className="carousel-indicators">
+					    <li data-target="#carouselIndicators2" data-slide-to="0" className="active"></li>
+					    <li data-target="#carouselIndicators2" data-slide-to="1"></li>
+					    <li data-target="#carouselIndicators2" data-slide-to="2"></li>
+					    <li data-target="#carouselIndicators2" data-slide-to="3"></li>
+					  </ol>
+					  <div className="carousel-inner" role="listbox">
+					    <div className="carousel-item active">
+					      <img className="d-block img-fluid" src="../img/nightlife.jpg" alt="Landing Page" />
+					    </div>
+					    <div className="carousel-item">
+					      <img className="d-block img-fluid" src="../img/cafe.jpg" alt="Logged-in Landing Page" />
+					    </div>
+					    <div className="carousel-item">
+					      <img className="d-block img-fluid" src="../img/tiger.jpg" alt="Data Entry Form" />
+					    </div>
+					    <div className="carousel-item">
+					      <img className="d-block img-fluid" src="../img/food1.jpg" alt="Visual Comparison" />
+					    </div>
+					  </div>
+					</div>
+
+				 </div>
 				<div className="btn new-link"> Submit New Link</div>
 				<div className="btn new-link"> Submit New Text Post</div>
 				<div className="reddit-gold"> daily reddit gold goal</div>
-				<div className="second-ad">  </div>
+				<div className="second-ad"> 
+    				<div id="carouselIndicators2" className="carousel slide" data-ride="carousel">
+					  <ol className="carousel-indicators">
+					    <li data-target="#carouselIndicators2" data-slide-to="0" className="active"></li>
+					    <li data-target="#carouselIndicators2" data-slide-to="1"></li>
+					    <li data-target="#carouselIndicators2" data-slide-to="2"></li>
+					    <li data-target="#carouselIndicators2" data-slide-to="3"></li>
+					  </ol>
+					  <div className="carousel-inner" role="listbox">
+					    <div className="carousel-item active">
+					      <img className="d-block img-fluid" src="../img/food2.jpg" alt="Landing Page" />
+					    </div>
+					    <div className="carousel-item">
+					      <img className="d-block img-fluid" src="../img/sydney.jpg" alt="Logged-in Landing Page" />
+					    </div>
+					    <div className="carousel-item">
+					      <img className="d-block img-fluid" src="../img/canoe.jpg" alt="Data Entry Form" />
+					    </div>
+					    <div className="carousel-item">
+					      <img className="d-block img-fluid" src="../img/fall.jpg" alt="Visual Comparison" />
+					    </div>
+					  </div>
+					</div>
+				 </div>
 			</div>
 		)
 	}
