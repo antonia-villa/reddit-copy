0d99b640ca1e4d65215a46e55d651cba219e6b7c
ANTONIA-VILLA”git config --global user.email AV.FRACTALWORK@GMAIL.COMSgit config --global user.email AV.FRACTALWORK@GMAIL.COM <av.fractalwork@gmail.com>
2018-01-25 17:34:37 -0800

    updated banner drop down

--- src/Banner.js
+++ src/Banner.js
@@ -1,8 +1,74 @@
 import React, {Component} from 'react';
 
+
 class Banner extends Component {
+	 constructor(props) {
+	    super(props);
+	    this.state = {
+	      values: [
+				"United States",
+				"AL",
+				"AK",
+				"AZ",
+				"AR",
+				"CA",
+				"CO",
+				"CT",
+				"DE",
+				"DC",
+				"FL",
+				"GA",
+				"HI",
+				"ID",
+				"IL",
+				"IN",
+				"IA",
+				"KS",
+				"KY",
+				"LA",
+				"ME",
+				"MT",
+				"NE",
+				"NV",
+				"NH",
+				"NJ",
+				"NM",
+				"NY",
+				"NC",
+				"ND",
+				"OH",
+				"OK",
+				"OR",
+				"MD",
+				"MA",
+				"MI",
+				"MN",
+				"MS",
+				"MO",
+				"PA",
+				"RI",
+				"SC",
+				"SD",
+				"TN",
+				"TX",
+				"UT",
+				"VT",
+				"VA",
+				"WA",
+				"WV",
+				"WI",
+				"WY"
+			]
+		};
+	}
+
 	render(){
+		    let optionTemplate = this.state.values.map(v => (
+      <option value={v}>{v}</option>
+    ));
+
 		return(
+			<div>
 			<div id="reddit-banner">
 				<img id="banner-img" src='./img/banner.png' />
 				<form className="login-form">
@@ -10,7 +76,7 @@ class Banner extends Component {
 					<div className="login-form-line">
 						<input className="login-info" type="text" value="username" placeholder="username" />
 						<input className="login-info" type="text" value="password" placeholder="password" />
-						<div class="remember-me">
+						<div className="remember-me">
 							<input type="checkbox" value="remember me" />
 							<a href="#">reset</a> 
 							<a href="#">password</a>
@@ -19,6 +85,16 @@ class Banner extends Component {
 					</div>
 				</form>
 			</div>
+				<div id="menuarea">
+					<span className="dropdown-title">popular in </span>
+					<span className="dropdown-title">selete state </span>
+			        <select value={this.state.value} onChange={this.handleChange}>
+			          {optionTemplate}
+			        </select>
+				</div>
+			</div>
+		
+
 		)
 	}
 }
